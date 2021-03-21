343c976169cafbb99aabc08ec69b687428c344f0
brunoaraujodeoliveira <brunimaraujodeoliveira@gmail.com>
2021-03-21 15:59:46 -0300

    Alteração configuração DB

---
+++ src/main/java/br/com/sgm/monitoramento/config/DatabaseConfig.java
@@ -0,0 +1,18 @@
+package br.com.sgm.monitoramento.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
+@Configuration
+public class DatabaseConfig {
+
+	@Bean
+	public MongoClient mongoClient() {
+		MongoClientURI uri = new MongoClientURI(
+				"mongodb+srv://monitoramento:monitoramento@cluster-tcc.26f15.mongodb.net/imagens?retryWrites=true&w=majority");
+		return new MongoClient(uri);
+	}
+}
