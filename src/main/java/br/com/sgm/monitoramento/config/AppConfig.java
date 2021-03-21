3115dd52dcf2566b89b94de915fb31ff3467cbc1
brunoaraujodeoliveira <brunimaraujodeoliveira@gmail.com>
2021-03-21 15:53:39 -0300

    Alteração configuração DB

--- src/main/java/br/com/sgm/monitoramento/config/AppConfig.java
+++ src/main/java/br/com/sgm/monitoramento/config/AppConfig.java
@@ -3,15 +3,26 @@ package br.com.sgm.monitoramento.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 
 @Configuration
 public class AppConfig {
 
 	@Bean
 	public MongoClient mongoClient() {
-		return MongoClients.create(
-				"mongodb+srv://monitoramento:monitoramento@cluster-tcc.26f15.mongodb.net/imagens?retryWrites=true&w=majority");
+        MongoClientOptions.Builder builder = new MongoClientOptions.Builder();
+        builder.maxConnectionIdleTime(60000);
+        MongoClientOptions opts = builder.build();
+
+        char[] password2 = "monitoramento".toCharArray();
+
+        MongoCredential credential2 = MongoCredential.createCredential("monitoramento", "imagens", password2);
+
+        MongoClient mongoClient = new MongoClient(new ServerAddress("cluster-tcc.26f15.mongodb.net/",27017), credential2, opts);
+
+	    return mongoClient;
 	}
 }
